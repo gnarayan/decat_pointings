a2ea4e56528590dbd985544b1fa340b65abf8f2c
Dillon Brout <djbrout@gmail.com>
2021-02-23 19:03:31 -0500

    fixed comma issue

--- make_json.py
+++ make_json.py
@@ -4,23 +4,29 @@ def individual(json_outpath,json_prefixs,pointRAs,pointDECs,obss,propids,objects
     for json_prefix,pointRA,pointDEC,obs,propid,tobject,exptype,program in zip(json_prefixs,pointRAs,pointDECs,obss,propids,objects,exptypes,programs):
         json_out = []
         json_out.append('[')
+        cntr = 0
+        lenobs = len(obs.split(','))
         for ob in obs.split(','):
+            cntr += 1
             filt = ob[0]
             exptime = ob[1:]
-            json_out.append('\t{')
+            json_out.append('\t{') 
             json_out.append('\t\t"count": 1,')
             json_out.append('\t\t"filter": "%s",'%filt)
             json_out.append('\t\t"exptime": %.1f,'%float(exptime.strip()))
             json_out.append('\t\t"RA": %f,' % round(float(pointRA), 5))
-            json_out.append('\t\t"dec": %f,' % round(float(pointDEC), 5))
+            json_out.append('\t\t"Dec": %f,' % round(float(pointDEC), 5))
             json_out.append('\t\t"object": "%s",'%tobject)
             json_out.append('\t\t"program": "%s",'%program)
             json_out.append('\t\t"expType": "%s",'%exptype)
             json_out.append('\t\t"note": "None",')
             json_out.append('\t\t"comment": "%s",'%json_prefix)
             json_out.append('\t\t"wait": "False",')
-            json_out.append('\t\t"propid": "%s",'%propid)
-            json_out.append('\t},')
+            json_out.append('\t\t"propid": "%s"'%propid)
+            if cntr == lenobs:
+                json_out.append('\t}')
+            else:            
+                json_out.append('\t},')
         json_out.append(']')
         
         np.savetxt(json_outpath+'/'+json_prefix+'.json', json_out, fmt='%s')

42d57c36327f3e871c75a55f95472937061a1b63
Dillon Brout <djbrout@gmail.com>
2021-02-25 18:06:11 -0500

    slewtime calculator

--- json_from_ysepz_target_list.py
+++ json_from_ysepz_target_list.py
@@ -6,10 +6,10 @@ from glob import glob
 import get_silicon as gs
 import os
 import make_json as mj
+import ccdmap
 
 def get_desired_ccd(x):
-    ccd = x.split('[')[1].split(']')[0][3:]
-    return  ccd
+    return ccdmap.ccdmap[x]
 
 def get_desired_observations(x):
     obs = x.split(';')[1].split(';')[0]
@@ -23,13 +23,12 @@ def get_propid(x):
     propid = x.split('{')[1].split('}')[0]
     return  propid
 
-
 def parse_infile(df):
     df['coords'] = df['ra_h'].astype(str)+' '+df['ra_m'].astype(str)+' '+df['ra_s'].astype(str)+' '+df['dec_d'].astype(str)+' '+df['dec_m'].astype(str)+' '+df['dec_s'].astype(str)
     coords = SkyCoord(df['coords'].to_numpy(), unit=(u.hourangle, u.deg))
-    df['radeg'] = coords.ra.degree
-    df['decdeg'] = coords.dec.degree
-    df['ccd'] = df['comment'].apply(get_desired_ccd)
+    df['candRA'] = coords.ra.degree
+    df['candDEC'] = coords.dec.degree
+    df['ccd'] = df['name'].apply(get_desired_ccd)
     df['obs'] = df['comment'].apply(get_desired_observations)
     df['priority'] = df['comment'].apply(get_priorities)
     df['propid'] = df['comment'].apply(get_propid)
@@ -59,16 +58,16 @@ for infile in infiles:
     df = pd.read_csv(outfilep,delim_whitespace=True)
     df = parse_infile(df)
     
-    fieldra,fielddec = gs.get_field_center_for_target_on_specific_ccd(df['radeg'],df['decdeg'],df['ccd'])
+    fieldra,fielddec = gs.get_field_center_for_target_on_specific_ccd(df['candRA'],df['candDEC'],df['ccd'])
     
     df['pointRA'] = fieldra
     df['pointDEC'] = fielddec
-    
 
     df['expTypes'] = 'object'
     df['programs'] = 'DECAT'
     
-    #df[['name','radeg','decdeg','ccd','obs','priority','pointRA','pointDEC']].to_csv('Feb19.csv')
+    df[['name','candRA','candDEC','ccd','obs','priority','pointRA','pointDEC']].to_csv(reformatdir+date+'.txt',sep=' ',index=False)
+    df[['name','candRA','candDEC']].to_csv(reformatdir+date+'_for_iObserve.txt',index=False,header=False,sep=' ')
 
-    mj.individual(json_outpath,df['name']+'_'+df['radeg'].round(3).astype(str)+'_'+df['decdeg'].round(3).astype(str)+'_PRIORITY'+df['priority'].astype(str),df['pointRA'],df['pointDEC'],df['obs'],df['propid'],df['name'],df['expTypes'],df['programs'])
+    mj.individual(json_outpath,df['name']+'_'+df['candRA'].round(3).astype(str)+'_'+df['candDEC'].round(3).astype(str)+'_PRIORITY'+df['priority'].astype(str),df['pointRA'],df['pointDEC'],df['obs'],df['propid'],df['name'],df['expTypes'],df['programs'])
 
---
+++ slewtimes.py
@@ -0,0 +1,57 @@
+import numpy as np
+import json as pyjson
+
+readout = 28.
+
+#from here http://www.ctio.noao.edu/noao/node/5826
+def slewtime_from_two_coords(ra1,dec1,ra2,dec2):
+    deg_separation = np.sqrt((ra1-ra2)**2+(dec1-dec2)**2)
+    return max([0.,20.-readout+220./100.*deg_separation])#subtracted off readout and bounded by zero
+
+def time_from_list_of_ras_decs_exptimes(ras,decs,exptimes):
+    ras,decs,exptimes = np.array(ras),np.array(decs),np.array(exptimes) 
+    totaltime = 0.
+    for i,ra,dec,exptime in zip(range(len(ras)),ras,decs,exptimes):
+        if i==0:
+            slewtime = 0
+        else:
+            slewtime = slewtime_from_two_coords(ra,dec,ras[i-1],decs[i-1])
+        totaltime += exptime+slewtime
+    return totaltime
+        
+def get_ras_decs_exptimes_from_json(json):
+    ras,decs,exptimes = [],[],[]
+    with open(json) as json_file: 
+        pointings = pyjson.load(json_file) 
+        for pointing in pointings:
+            ras.append(pointing['RA'])
+            decs.append(pointing['Dec'])
+            exptimes.append(pointing['exptime'])
+    return ras,decs,exptimes
+
+def time_for_single_json(json):
+    #json: filename
+    ras,decs,exptimes = get_ras_decs_exptimes_from_json(json)
+    totaltime = time_from_list_of_ras_decs_exptimes(ras,decs,exptimes)
+    return totaltime
+
+def total_time_from_jsons(jsons):
+    #jsons: list of filenames
+    ras,decs,exptimes = [],[],[]
+    for json in jsons:
+        tras,tdecs,texptimes = get_ras_decs_exptimes_from_json(json)
+        ras.extend(tras)
+        decs.extend(tdecs)
+        exptimes.extend(texptimes)
+    totaltime =	time_from_list_of_ras_decs_exptimes(ras,decs,exptimes)
+    return totaltime
+        
+
+#USAGE:
+#t = time_for_single_json('/Users/djbrout/Dropbox/decat_pointings/jsons/20210223/2021dep_211.053_27.626_PRIORITY2.json')
+#print(t)
+#t = total_time_from_jsons(['/Users/djbrout/Dropbox/decat_pointings/jsons/20210223/2021dep_211.053_27.626_PRIORITY2.json',
+#                           '/Users/djbrout/Dropbox/decat_pointings/jsons/20210223/2021dnl_221.762_0.476_PRIORITY2.json',
+#                           '/Users/djbrout/Dropbox/decat_pointings/jsons/20210223/2021dov_134.078_0.442_PRIORITY1.json'])
+#print(t)
+

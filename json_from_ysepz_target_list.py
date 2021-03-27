1c8fec91821ab2ee30d9f65b10ac0802de40e8b7
Dillon Brout <djbrout@gmail.com>
2021-03-27 00:00:11 -0400

    added fieldmaps

--- json_from_ysepz_target_list.py
+++ json_from_ysepz_target_list.py
@@ -70,5 +70,5 @@ for infile in infiles:
     df[['name','candRA','candDEC','ccd','obs','priority','pointRA','pointDEC']].to_csv(reformatdir+date+'.txt',sep=' ',index=False)
     df[['name','candRA','candDEC']].to_csv(reformatdir+date+'_for_iObserve.txt',index=False,header=False,sep=' ')
 
-    mj.individual(json_outpath,df['name']+'_'+df['candRA'].round(3).astype(str)+'_'+df['candDEC'].round(3).astype(str)+'_PRIORITY'+df['priority'].astype(str),df['pointRA'],df['pointDEC'],df['obs'],df['propid'],df['name']+'_P'+df['priority'].astype(str),df['expTypes'],df['programs'])
+    mj.individual(json_outpath,df['name']+'_'+df['candRA'].round(0).astype(str)+'_'+df['candDEC'].round(0).astype(str)+'_PRIORITY'+df['priority'].astype(str),df['pointRA'],df['pointDEC'],df['obs'],df['propid'],df['name']+'_P'+df['priority'].astype(str),df['expTypes'],df['programs'])
 

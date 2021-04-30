1628e01a3954d981246bf943b6213f86a7cfdd6c
Dillon Brout <djbrout@gmail.com>
2021-04-30 11:04:05 -0400

    debass automation

---
+++ makeobservabilityplot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from astropy.time import Time
+from astroplan import FixedTarget, Observer
+from astroplan.plots import plot_airmass
+from astropy import units as u
+from astropy.coordinates import SkyCoord, get_moon, EarthLocation
+
+def doplot(timestr='2021-04-29',ra=125.203408,dec=-12.598140,name='2021koj',site='CTIO',block=True):
+    
+    time = Time(timestr) + 24*u.hour + np.linspace(-3.0, 14, 100)*u.hour
+
+    apo = Observer.at_site(site)
+    
+    SN_coord = SkyCoord(ra=ra*u.deg, dec=dec*u.deg)
+    moon_coord = get_moon(time,location=EarthLocation.of_site('CTIO'))
+    
+    sn_moon_sep = min(moon_coord.separation(SN_coord).degree)
+    
+    SN_target = FixedTarget(coord=SN_coord, name='%s: %d deg moon sep'%(name,sn_moon_sep))
+    moon_target = FixedTarget(coord=moon_coord, name='Moon')
+
+    moon_styles = {'linestyle': ':', 'color': 'grey'}
+    SN_styles = {'color': 'k', 'linewidth': 3}
+    
+    plot_airmass(moon_target, apo, time, brightness_shading=True, altitude_yaxis=True,style_kwargs=moon_styles)
+    plot_airmass(SN_target, apo, time, brightness_shading=True, altitude_yaxis=True,style_kwargs=SN_styles)
+    
+    plt.legend(shadow=True,loc=4)
+    plt.tight_layout()
+    plt.show(block=block)
+
+if __name__ == '__main__':    
+    doplot()
---
+++ prepare_debass.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import readobslogs as ro
+import makeobservabilityplot as mop
+from datetime import datetime
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+import matplotlib.pyplot as plt
+
+def split(word):
+    return [char for char in word]
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+creds = ServiceAccountCredentials.from_json_keyfile_name('.gclient.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open("DEBASS Sample").sheet1
+
+list_of_lists = sheet.get_all_values()
+
+goodcollist = ['snid','Following?','TNS class','RA','DEC']
+colnames = list_of_lists[0]
+dict = {}
+for i,col in enumerate(colnames):
+    if col in goodcollist:
+        dict[col] = sheet.col_values(i+1)[1:]
+
+df = pd.DataFrame.from_dict(dict)
+
+from datetime import datetime
+currdate=datetime.today().strftime('%Y-%m-%d')
+datestr = input(f'Please Enter Observing Date in the following format (YYYY-MM-DD)\ndefault {currdate}\n')
+if datestr == '': datestr=currdate
+obsdict = ro.run(verbose=False)
+
+for i,row in df.iterrows():
+    if 'FINISHED' in row['Following?']: continue
+    if 'ABANDON' in row['Following?']:	continue
+    if 'LOST' in row['Following?']:  continue
+    if '91' in row['TNS class']:  continue
+    if ('ia' in row['TNS class'].lower()) | ('?' in row['TNS class']):
+        print(obsdict[row['snid']])
+        mop.doplot(datestr,ra=float(row['RA']),dec=float(row['DEC']),name=row['snid'],block=False)
+        priority = input('Please enter a priority for this object (ie 1 2 3)\n')
+        filters = split(input('Please enter a priority for this object (ie griz)\n'))
+        plt.clf()

f256e8959fdb1d5ec0cb9015afed8d488d350076
Dillon Brout <djbrout@gmail.com>
2021-04-30 22:29:53 -0400

    update

--- prepare_debass.py
+++ prepare_debass.py
@@ -9,6 +9,7 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 import matplotlib.pyplot as plt
 import os
+import editjson as ej
 
 def split(word):
     return [char for char in word]
@@ -38,7 +39,10 @@ datestr = input(f'Please Enter Observing Date in the following format (YYYY-MM-D
 if datestr == '': datestr=currdate
 obsdict = ro.run(verbose=False)
 
+
+skiprows = 60
 for i,row in df.iterrows():
+    if i < skiprows: continue
     if 'FINISHED' in row['Following?']: continue
     if 'ABANDON' in row['Following?']:	continue
     if 'LOST' in row['Following?']:  continue
@@ -49,7 +53,16 @@ for i,row in df.iterrows():
         mop.doplot(datestr,ra=float(row['RA']),dec=float(row['DEC']),name=row['snid'],block=False)
         priority = input('Please enter a priority for this object (ie 1 2 3)\n')
         filters = split(input('Please enter filters for this object (ie griz)\n'))
+        exptimes = []
         for f in filters:
             exptime = input(f'Enter Exptime {f} (default 15)\n')
             if exptime == '': exptime = '15'
+            exptimes.append(exptime)
         plt.clf()
+        if os.path.exists('jsons/2020B-0053_DEBASS_Brout/BASE/%s.json'%row['snid']):
+            os.system('rm jsons/2020B-0053_DEBASS_Brout/EVERYTHING/%s_P*.json'%(row['snid']))
+            ej.edit('jsons/2020B-0053_DEBASS_Brout/BASE/%s.json'%row['snid'],priority,filters,exptimes,
+                 'jsons/2020B-0053_DEBASS_Brout/EVERYTHING/%s_P%s.json'%(row['snid'],priority))
+        else:
+            print('WARNING: jsons/2020B-0053_DEBASS_Brout/BASE/%s.json'%row['snid']+'\nDoes Not Exist')
+            input('press enter to continue')

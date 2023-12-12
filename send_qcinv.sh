f34bc50a7479af77a2fa22911ef3ef404b255b50
djbrout <djbrout@gmail.com>
2023-12-12 10:18:42 -0500

    Delete send_qcinv.sh

--- send_qcinv.sh
+++
@@ -1,7 +0,0 @@
-qcinvfile=$1
-
-
-echo "uploading now, please wait..."
-
-curl -F file=@$qcinvfile -F "initial_comment=qcInv" -F channels=C069PPDNQ2H -H "Authorization: Bearer xoxb-1734759934679-6355554410112-0kiSsFIH7zqebY6hkSuPvATO" https://slack.com/api/files.upload > upload.log
-

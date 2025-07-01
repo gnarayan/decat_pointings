61e12fbc1078472d834662a6c040026b3f04955f
Armin Rest <arest@stsci.edu>
2025-07-01 14:53:49 -0400

    E* order

--- gen_obsplan/obsplan.sh
+++ gen_obsplan/obsplan.sh
@@ -1,4 +1,15 @@
 #export OBSPLAN_ROOTDIR="/Users/arest/observing/decat-obsplan-generator"
-export OBSPLAN_ROOTDIR="/Users/arest/observing/decat_pointings"
-alias cdjson='cd $OBSPLAN_ROOTDIR/json_files'
-alias cdobsplan='cd $OBSPLAN_ROOTDIR/obsplans'
+if [[ $HOSTNAME =~ arminmac* ]] || [[  $HOSTNAME =~ lswlan* ]]; then
+	export OBSPLAN_ROOTDIR="/Users/arest/observing/decat_pointings"
+	alias cdjson='cd $OBSPLAN_ROOTDIR/json_files'
+	alias cdobsplan='cd $OBSPLAN_ROOTDIR'
+elif [[ $HOSTNAME =~ plhstproc* ]]; then
+	export OBSPLAN_ROOTDIR="/astro/armin/observing/decat_pointings"
+	alias cdjson='cd $OBSPLAN_ROOTDIR/json_files'
+	alias cdobsplan='cd $OBSPLAN_ROOTDIR'
+else
+   echo "Hostname $HOSTNAME is not defined yet in the sourceme file!"
+   return 1;
+fi
+
+

45f3701be5baa69aeeff0117b51f559ca0b8cae5
DECam Observer <DECamObserver@observer4.ctio.noao.edu>
2026-02-08 06:24:37 -0300

    adding qcinv

---
+++ upload_scripts/upload_qcinv_2026A.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#COMMAND LINE ARGUMENT SHOULD BE DATE OF START OF OBSERVATIONS YYMMDD eg 231213
+scp  DECamObserver@observer2.ctio.noao.edu:/user/DECamObserver/20$1.qcinv  ./2026A/$1/
+git add ./2026A/$1/20$1.qcinv
+git commit -am 'adding qcinv'
+git push 
